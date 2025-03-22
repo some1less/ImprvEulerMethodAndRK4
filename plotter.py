e8db756e933442ceb9d46668a5e4cce1e7df51e5
some1less <muracks.max@gmail.com>
2025-03-22 23:36:11 +0100

    Added option var to simulate and graph methods to launch and display selected method

--- plotter.py
+++ plotter.py
@@ -1,7 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def plot_energies(energies):
+def plot_energies(energies, option):
     # wide_format -> long_format in seaborn
     df_melt = energies.melt(id_vars='Time (s)',
                       value_vars=['Kinetic Energy', 'Potential Energy', 'Total Energy'],
@@ -12,7 +12,10 @@ def plot_energies(energies):
     sns.set(style="whitegrid")
     plt.figure(figsize=(10, 6))
     sns.lineplot(data=df_melt, x='Time (s)', y='Energy', hue='Energy Type')
-    plt.title("Dependency energy/time(secs) (RK4 method)")
+    if option == 'rk2':
+        plt.title("Dependency energy/time(secs) (improved Euler's method)")
+    elif option == 'rk4':
+        plt.title("Dependency energy/time(secs) (RK4 method)")
     plt.xlabel("Time (s)")
     plt.ylabel("Energy")
     plt.show()
--- simulation.py
+++ simulation.py
@@ -1,9 +1,9 @@
-import math
 import pandas as pd
 from pendulum import energies
 from rk4 import rk4_step
+from rk2 import rk2_step
 
-def run_simulation(a0, w0, dt, n_steps):
+def run_simulation(a0, w0, dt, n_steps, option):
     times = []
     E_kinetic = []
     E_potential = []
@@ -14,14 +14,27 @@ def run_simulation(a0, w0, dt, n_steps):
     time = 0
 
     # simulation
-    for i in range(n_steps):
-        times.append(time)
-        E_k, E_p, E_tot = energies(a, w)
-        E_kinetic.append(E_k)
-        E_potential.append(E_p)
-        E_total.append(E_tot)
-        a, w = rk4_step(a, w, dt)
-        time += dt
+    if option == 'rk4':
+
+        for i in range(n_steps):
+            times.append(time)
+            E_k, E_p, E_tot = energies(a, w)
+            E_kinetic.append(E_k)
+            E_potential.append(E_p)
+            E_total.append(E_tot)
+            a, w = rk4_step(a, w, dt)
+            time += dt
+
+    elif option == 'rk2':
+
+        for i in range(n_steps):
+            times.append(time)
+            E_k, E_p, E_tot = energies(a, w)
+            E_kinetic.append(E_k)
+            E_potential.append(E_p)
+            E_total.append(E_tot)
+            a, w = rk2_step(a, w, dt)
+            time += dt
 
     # DataFrame creation for comfortable usage
     df = pd.DataFrame({
