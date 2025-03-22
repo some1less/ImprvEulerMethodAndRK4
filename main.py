c89da56552977e4285feb6a267469b5b0763ff93
some1less <muracks.max@gmail.com>
2025-03-22 23:38:32 +0100

    Updated main class for comfortable setting up the program

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import math
 from plotter import plot_energies
 from simulation import run_simulation
 
-
 a0 = math.radians(30) # angle between edge positions
 w0 = 0.1 # angular speed
 dt = 0.01
-n_steps = 1000     # number of steps (e.g 10 sec)
+n_steps = 10000     # number of steps (10 sec)
 
-df = run_simulation(a0, w0, dt, n_steps)
+option = 'rk2'
+# You have two options:
+# 1. rk2 (improved Euler's method);
+# 2. rk4
 
-plot_energies(df)
+df = run_simulation(a0, w0, dt, n_steps, option)
+plot_energies(df, option)
