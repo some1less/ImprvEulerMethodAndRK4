40fc146d68b88545d1217e12a3ff935b1cab3d4f
some1less <muracks.max@gmail.com>
2025-03-22 20:39:08 +0100

    Updated (normalized) main class

--- main.py
+++ main.py
@@ -1,45 +1,14 @@
 import math
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
 
-from pendulum import energies
 from plotter import plot_energies
-from rk4 import *
+from simulation import run_simulation
+
 
 a0 = math.radians(30) # angle between edge positions
 w0 = 0.1 # angular speed
-
-
-
 dt = 0.01
-n_steps = 10000     # number of steps (e.g 10 sec)
-times = []
-E_kinetic = []
-E_potential = []
-E_total = []
+n_steps = 1000     # number of steps (e.g 10 sec)
 
-
-a = a0
-w = w0
-time = 0
-
-# simulation
-for i in range(n_steps):
-    times.append(time)
-    E_k, E_p, E_tot = energies(a, w)
-    E_kinetic.append(E_k)
-    E_potential.append(E_p)
-    E_total.append(E_tot)
-    a, w = rk4_step(a, w, dt)
-    time += dt
-
-# DataFrame creation for comfortable usage
-df = pd.DataFrame({
-    'Time (s)': times,
-    'Kinetic Energy': E_kinetic,
-    'Potential Energy': E_potential,
-    'Total Energy': E_total
-})
+df = run_simulation(a0, w0, dt, n_steps)
 
 plot_energies(df)
